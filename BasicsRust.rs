a559f2ac7894eb1c5f154ed4b39e190fc5ad7ab7
Vishal Chauhan <vshlchauhab@gmail.com>
2022-09-06 15:01:30 +0530

    Update BasicsRust.rs

--- BasicsRust.rs
+++ BasicsRust.rs
@@ -1,7 +1,22 @@
-fn main() {
-name(2,5);
-}
-fn name(x:i32,y:i32){
+// fn main() {
+// name(2,5);
+// }
+// fn name(x:i32,y:i32){
     
-    println!("x is  {} and y is {}",x,y);
-}
+//     println!("x is  {} and y is {}",x,y);
+// }
+
+
+//Mermory safety
+
+// fn main() {
+
+// let ans;
+// if true {
+//     ans=2;
+// }
+// else{
+//   println!("hi")
+// }
+// println!("{}",ans)
+// }
